f3756050194572eef7413fb1fa1cb52513c0c5a6
Bugagashenki555 <106430213+Bugagashenki555@users.noreply.github.com>
2022-05-28 21:01:28 +1000

    Add files via upload

---
+++ laba6/Model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace laba6
+{
+    class Model
+    {
+        public int calculateMounth(string vibMounth,string[] mounthArray)
+        {
+            DateTime currentDate = DateTime.Now;
+            int result = Math.Abs(currentDate.Month - (Array.IndexOf(mounthArray,vibMounth) + 1));
+            return result;
+
+        }
+    }
+}
---
+++ laba6/ViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Linq;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using System.Data;
+using System.ComponentModel;
+
+namespace laba6
+{
+    class ViewModel : INotifyPropertyChanged
+    {
+        public string vibMounth;
+        public string[] mounthArr = {"январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь" };
+        public event PropertyChangedEventHandler PropertyChanged;
+        private RelayCommand clickCommand;
+        private int result;
+        protected void RaisePropertyChanged(string p)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(p));
+            }
+        }
+        public ViewModel()
+        {
+
+        }
+
+        public string Mounth2
+        {
+            get
+            {
+                return vibMounth;
+            }
+            set
+            {
+                vibMounth = value;
+                RaisePropertyChanged("Mounth2");
+            }
+        }
+        public string[] getMounth
+        {
+             get {
+                return mounthArr;
+             }
+        }
+        
+
+        public int Result
+        {
+            get
+            {
+                return result;
+            }
+            set
+            {
+                result = value;
+                RaisePropertyChanged("Result");
+            }
+        }
+
+        public RelayCommand ClickCommand
+        {
+            get
+            {
+                return clickCommand ?? (clickCommand = new RelayCommand(obj =>
+                {
+                    Model mounth = new Model();
+                    Result = mounth.calculateMounth( vibMounth, mounthArr);
+                }));
+            }
+        }
+    }
+}
